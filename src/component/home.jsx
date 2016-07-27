d8d48397d93aa73951e76497492ba863bbf221d2
Kurtis Young <kcyoung1@ualberta.ca>
2016-07-27 09:56:35 -0700

    adding constructor to react component

---
+++ src/component/home.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, Redirect} from 'react-router';
+
+export default class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      comics : [],
+    }
+  }
+  
+}
