c510037a10b8fe9e1f6287e7108db077fe486137
Kurtis Young <kcyoung1@ualberta.ca>
2016-07-27 11:25:26 -0700

    applying the show/hide function onto the description box, working on making it for each individual comic book

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -28,7 +28,7 @@ export default class Home extends React.Component {
   }
 
   _showInfo(comic) {
-    this.setState({ show: true });
+    this.setState({ show: !this.state.show });
   }
 
   render() {
@@ -43,7 +43,7 @@ export default class Home extends React.Component {
               { comics.map((comic, index) => (
                   <div key={index} className="comic-image" onClick={this._showInfo.bind(this)}>
                       <img src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt={comic.title} />
-                      {this.state.show ? <div className="comic-desc">{comic.description}</div> : "" }
+                      {this.state.show ? <div className="comic-desc">{comic.description}</div> : " " }
                   </div>
               ))}
             </div>
