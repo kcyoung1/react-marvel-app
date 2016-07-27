4a0ca6fef749942e88e269600d7d730a74ae537b
Kurtis Young <kcyoung1@ualberta.ca>
2016-07-27 10:02:54 -0700

    renaming components file to match route and created a AJAX request function and console logging the results

---
+++ src/components/home.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, Redirect} from 'react-router';
+
+export default class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      comics : [],
+    }
+  }
+
+  componentWillMount() {
+    this._fetchData();
+  }
+
+  _fetchData()  {
+    $.ajax({
+      method: 'GET',
+      url:
+      'http://gateway.marvel.com/v1/public/comics?apikey=c1a92559e851c361fc7f540560163ef8',
+      success: function(result) {
+        console.log(result);
+      }
+    });
+  }
+
+  render() {
+    
+  }
+}
